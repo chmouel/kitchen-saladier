3e87c76aac510bb1273197d6201fcc2904732127
Chmouel Boudjnah <chmouel.boudjnah@enovance.com>
2014-10-24 19:04:19 +0200

    Readd the functional test
    Make them more like smoketests at least just to make sure our server is
    actually starting properly. We will expand it with tempest-lib and such
    in the future but for now as smoektest it's good enough.
    
    Change-Id: I11ce226374280110773a549b9831210921ce0117

--- tools/container/functional/start.sh
+++ tools/container/functional/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 function check_up() {
     service=$1
@@ -29,11 +29,36 @@ function check_up() {
 check_up saladier ${SALADIER_PORT_8777_TCP_ADDR} 8777
 check_up keystone ${KEYSTONE_PORT_35357_TCP_ADDR} 35357
 
+function exit_it {
+    [[ $? != 0 ]] && echo "has failed :("
+}
+
+trap exit_it EXIT
+
+
+echo "Starting functional testing"
+echo "---------------------------"
+
+echo -n "Getting Token from Keystone: "
 TOKEN=$(curl -s -X POST http://${KEYSTONE_PORT_35357_TCP_ADDR}:35357/v2.0/tokens -H "Content-Type: application/json" \
              -d '{"auth": {"tenantName": "service", "passwordCredentials": {"username": "saladier", "password": "password"}}}' |
             python -c 'import sys, json; print json.load(sys.stdin)["access"]["token"]["id"]')
+echo "OK."
 
-# This is my only unittest for now we will expand with a proper unittest
+
+# This is our only unittest for now we will expand with a proper unittest
 # framework in the future when REST class will be written. (The -f to curl would
 # exit 1 if we have a 4xx or 5xx errors)
-curl -s -L -f -H "x-auth-token: ${TOKEN}" http://${SALADIER_PORT_8777_TCP_ADDR}:8777/v1/test
+
+# Create a product
+echo -n "Creating a product: "
+curl -f -s -L -H "x-auth-token: $TOKEN" -X POST -d 'name=ttttt' -d 'team=boa' -d 'contact=thecedric@isthegreatest.com' \
+     http://${SALADIER_PORT_8777_TCP_ADDR}:8777/v1/products/
+echo "OK"
+
+echo -n "Get created product: "
+curl -f -s -L -H "x-auth-token: $TOKEN" http://${SALADIER_PORT_8777_TCP_ADDR}:8777/v1/products/ | grep -q "thecedric@isthegreatest.com"
+echo "OK."
+
+
+echo "Done and successfull :)"
