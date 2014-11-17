ce3f597323ee72a8420c73f0b132fad6b5e259c2
Chmouel Boudjnah <chmouel.boudjnah@enovance.com>
2014-11-17 13:00:10 +0100

    Remove Authors from copyright
    This is enforced in latest hacking/flake8 and at least that one rule i
    can agree with (and it's my fault).
    
    Generated with the always helpful sed :
    
     sed -i -e '/# Author.*/,/^#/d' saladier/***/*py
    
    Change-Id: I81194df8aafef95de30ac3857788edc6b35b1e82

--- saladier/service.py
+++ saladier/service.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Author: Chmouel Boudjnah <chmouel.boudjnah@enovance.com>
-#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
